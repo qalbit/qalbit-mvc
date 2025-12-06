351b52cd7c1ce3b85f65438cf67a97c5fa42e426
Abidhusain Chidi <abidhusain@qalbit.com>
2025-12-06 21:40:40 +0530

    FR-A1: Added Favicons.

--- resources/views/partials/head.php
+++ resources/views/partials/head.php
@@ -31,6 +31,13 @@ $jsonLd = $jsonLd ?? null;
 
 <title><?= htmlspecialchars($title) ?></title>
 
+<link rel="icon" type="image/png" href="<?= asset('/favicon-96x96.png') ?>" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="<?= asset('/favicon.svg') ?>" />
+<link rel="shortcut icon" href="<?= asset('/favicon.ico') ?>" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?= asset('/apple-touch-icon.png') ?>" />
+<meta name="apple-mobile-web-app-title" content="QalbIT" />
+<link rel="manifest" href="<?= asset('/site.webmanifest') ?>" />
+
 <?php if (!empty($description)): ?>
     <meta name="description" content="<?= htmlspecialchars($description) ?>">
 <?php endif; ?>
